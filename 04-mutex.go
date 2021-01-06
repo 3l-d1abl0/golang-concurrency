305f6b4af3c5abd9a1f8da6064005e8cfe02ee6f
3l-d1abl0 <sameer.barha12@gmail.com>
2021-01-06 11:28:11 +0530

    mutexes and locks

---
+++ 04-mutex.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+type Counter struct {
+	sync.Mutex
+	value int
+}
+
+func main() {
+
+	counter := Counter{}
+	for i := 0; i < 100; i++ {
+
+		go func(i int) {
+			//counter.Lock()
+			counter.value++
+			//defer counter.Unlock()
+		}(i)
+	}
+
+	time.Sleep(time.Second)
+	counter.Lock()
+	println(counter.value)
+	defer counter.Unlock()
+
+}
+
+//go run -race <>
+//GOMAXPROCS=1
+go run -race main.go
